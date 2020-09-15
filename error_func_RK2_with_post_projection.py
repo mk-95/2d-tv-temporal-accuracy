fixed the stability computation for the RK2 case

error_func_RK2_with_post_projection.py
@@ -82,7 +82,8 @@ def error_RK2_with_post_projection(steps=3, return_stability=False, name='heun',
     ken_exact = np.sum(uexc_cc.ravel() ** 2 + vexc_cc.ravel() ** 2) / 2
     ken_old = ken_new
     final_KE = nx * ny
-    target_ke = ken_exact - alpha * (ken_exact - final_KE)
+    # target_ke = ken_exact - alpha * (ken_exact - final_KE)
+    target_ke = final_KE + (1 - alpha)*(ken_new-final_KE)
     print('time = ', t)
     print('ken_new = ', ken_new)
     print('ken_exc = ', ken_exact)
@@ -181,25 +182,39 @@ def error_RK2_with_post_projection(steps=3, return_stability=False, name='heun',
 
         # compute of kinetic energy
         ken_new = np.sum(ucc.ravel() ** 2 + vcc.ravel() ** 2) / 2
-        ken_exact = np.sum(uexc_cc.ravel() ** 2 + vexc_cc.ravel() ** 2) / 2
-        print('time = ', t)
-        print('ken_new = ', ken_new)
-        print('target_ken=', target_ke)
-        print('ken_exc = ', ken_exact)
-        print('(ken_new - ken_old)/ken_old = ', (ken_new - ken_old) / ken_old)
-        if (((ken_new - ken_old) / ken_old) > 0 and count > 1) or np.isnan(ken_new):
+
+        difference = ken_new - ken_old
+
+        if (ken_new<target_ke):
+            is_stable=True
+            stability_counter+=1
+
+        diff = 100 * difference /target_ke
+        if (diff > 10): stability_counter += 1
+
+        if (((count > 1) and (stability_counter > 5)) or np.isnan(ken_new)):
             is_stable = False
-            print('is_stable = ', is_stable)
-            if stability_counter > 5:
-                print('not stable !!!!!!!!')
-                break
-            else:
-                stability_counter += 1
-        else:
-            is_stable = True
-            print('is_stable = ', is_stable)
-            if ken_new < target_ke and count > 30:
-                break
+            print('not stable !!!!!!!!')
+            break
+
+        # print('time = ', t)
+        # print('ken_new = ', ken_new)
+        # print('target_ken=', target_ke)
+        # print('ken_exc = ', ken_exact)
+        # print('(ken_new - ken_old)/ken_old = ', (ken_new - ken_old) / ken_old)
+        # if (((ken_new - ken_old) / ken_old) > 0 and count > 1) or np.isnan(ken_new):
+        #     is_stable = False
+        #     print('is_stable = ', is_stable)
+        #     if stability_counter > 5:
+        #         print('not stable !!!!!!!!')
+        #         break
+        #     else:
+        #         stability_counter += 1
+        # else:
+        #     is_stable = True
+        #     print('is_stable = ', is_stable)
+        #     if ken_new < target_ke and count > 30:
+        #         break
         ken_old = ken_new.copy()
         print('is_stable = ', is_stable)
 

tv_stability.py
@@ -12,11 +12,13 @@ tend = 100000
 U = 1.42
 n = 15
 Res = [i for i in np.logspace(-1,2,n)]
+# Res = [0.5]
 def run_fname():
     nsteps = int(tend/probDescription.get_dt())
     # here add your scheme:
     #------------------------
-    is_stable = error_RK2(steps=nsteps,return_stability=True, name='heun', guess='first', project=[0],alpha=0.999)
+    is_stable = error_RK2(steps=nsteps,return_stability=True, name='heun', guess=None, project=[1],alpha=0.999)
+    # is_stable = error_RK2(steps=nsteps,return_stability=True, name='heun', guess='first', project=[0],alpha=0.999)
 
 
     print('is_stable = {}'.format(is_stable))
@@ -44,6 +46,7 @@ def bisect_CFL(CFLs,mu,old_CFL_low=0,old_is_stable_low=0,tol=1e-2):
     diff = CFL_H-CFL_L
     midpoint = (CFL_L+CFL_H)/2
 
+    probDescription.set_mu(mu)
     probDescription.set_dt(dt_low)
     is_stable_low = run_fname() if old_CFL_low!=CFL_L else old_is_stable_low
     probDescription.set_dt(dt_high)
@@ -78,7 +81,7 @@ integ = "RK21"
 for Re in Res:
     CFL_max,CFL_min = (4,0.1)
     dx = 1/32
-    mu = U*dx/1.0
+    mu = U*dx/Re
     CFL,err,stable_low, stable_high = bisect_CFL([CFL_max,CFL_min],mu)
     with open("{}-stability.txt".format(integ), "a") as myfile:
         myfile.write("{},{},{},{},{}".format(CFL,Re,err,stable_low, stable_high))